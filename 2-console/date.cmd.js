474cb1fa0281c93b0fe41a2788bee7ad927b9e3d
nikita-o <nik.oschepkov@yandex.ru>
2022-05-31 21:51:32 +0300

    fix date

--- 2-console/date.cmd.js
+++ 2-console/date.cmd.js
@@ -3,76 +3,44 @@
 const yargs = require('yargs/yargs');
 const { hideBin } = require('yargs/helpers');
 
-// const getParam = (param) => param.year ? 'y' : param.mount ? 'm' : param.date ? 'd' : null;
-
-function getParam(param) {
-    if (param.year)
-        return 'y'
-    if (param.month)
-        return 'm'
-    if (param.date)
-        return 'd'
-}
-
-function getDate(format, shift) {
-    let date = new Date();
-
-    switch (shift?.format) {
-        case 'y':
-            date.setFullYear(date.getFullYear() + shift.step);
-            break;
-        case 'm':
-            date.setMonth(date.getMonth() + shift.step);
-            break;
-        case 'd':
-            date.setDate(date.getDate() + shift.step);
-            break;
-    }
-
-    switch (format) {
-        case 'y':
-            return date.getFullYear();
-        case 'm':
-            return date.getMonth() + 1;
-        case 'd':
-            return date.getDate();
-        default:
-            return date + '';
-    }
-}
-
 yargs(hideBin(process.argv))
 .options({
     'year': {
         alias: 'y',
-        type: 'boolean',
         description: 'год'
     },
     'month': {
         alias: 'm',
-        type: 'boolean',
         description: 'месяц'
     },
     'date': {
         alias: 'd',
-        type: 'boolean',
         description: 'дата'
     },
 })
 .command('current', 'Текущая дата', () => {}, (argv) => {
-    const format = getParam(argv);
-    console.log(getDate(format));
+    const date = new Date();
+    if (argv.year)
+        console.log(date.getFullYear());
+    else if (argv.month)
+        console.log(date.getMonth() + 1);
+    else if (argv.date)
+        console.log(date.getDate())
+    else
+        console.log(date + '');
 })
-.command('add [step]', 'Дата в будущем', () => {}, (argv) => {
-    console.log(getDate(null, {
-        format: getParam(argv),
-        step: argv.step
-    }));
+.command('add', 'Дата в будущем', () => {}, (argv) => {
+    const date = new Date();
+    date.setFullYear(date.getFullYear() + (argv.y ?? 0));
+    date.setMonth(date.getMonth()       + (argv.m ?? 0));
+    date.setDate(date.getDate()         + (argv.d ?? 0));
+    console.log(date + '');
 })
-.command('sub [step]', 'Дата в прошлом', () => {}, (argv) => {
-    console.log(getDate(null, {
-        format: getParam(argv),
-        step: -argv.step
-    }));
+.command('sub', 'Дата в прошлом', () => {}, (argv) => {
+    const date = new Date();
+    date.setFullYear(date.getFullYear() - (argv.y ?? 0));
+    date.setMonth(date.getMonth()       - (argv.m ?? 0));
+    date.setDate(date.getDate()         - (argv.d ?? 0));
+    console.log(date + '');
 })
 .argv
